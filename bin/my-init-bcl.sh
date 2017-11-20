39a3568ec4b796b496c55c5e0c5ac609e9d9e0bc
Luca Pireddu <pireddu@jic-md29.jicsardegna.it>
2017-11-20 16:10:55 +0100

    Initial commit

---
+++ bin/my-init-bcl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -x
+
+hdfs namenode -format
+/home/cesco/dump/hadoop/sbin/start-dfs.sh
+#/home/cesco/dump/hadoop/sbin/start-yarn.sh
+#hadoop distcp file:///home/cesco/data/illumina.{tiny,small} /data/
+/home/cesco/bin/mypsh "mkdir -p /tmp/flink-tasktmp"
+/home/cesco/bin/mypsh "rsync -Pra /home/cesco/data/ref/ /tmp/ref/"
+/home/cesco/dump/flink/bin/start-cluster.sh
