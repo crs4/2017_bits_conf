b25a7eacbd9a4b1d59b4fcc85f77e65ac9784f5f
Luca Pireddu <pireddu@crs4.it>
2017-11-24 17:18:35 +0100

    Various fixes and generalizations to utility scripts

--- bin/my-init-bcl.sh
+++ bin/my-init-bcl.sh
@@ -3,10 +3,20 @@ set -x
 set -o errexit
 set -o nounset
 
-/home/cesco/dump/hadoop/bin/hdfs namenode -format
-/home/cesco/dump/hadoop/sbin/start-dfs.sh
+master="$(cat "${BITSCONF}/master")"
+
+if [ "${HOSTNAME}" != "${master}" ]; then
+  echo "This host '${HOSTNAME}' is not configured as master (currently ${master})." >&2
+  echo "This script needs to be run from the master node" >&2
+  exit 1
+fi
+
+hdfs namenode -format
+start-dfs.sh
+#start-yarn.sh
 #/home/cesco/dump/hadoop/sbin/start-yarn.sh
 #hadoop distcp file:///home/cesco/data/illumina.{tiny,small} /data/
-/home/cesco/bin/mypsh "mkdir -p /tmp/flink-tasktmp"
-/home/cesco/bin/mypsh "rsync -Pra /home/cesco/data/ref/ /tmp/ref/"
-/home/cesco/dump/flink/bin/start-cluster.sh
+#LP seems unused mypsh "mkdir -p /tmp/flink-tasktmp"
+#mypsh "rsync -Pra /home/cesco/data/ref/ /tmp/ref/"
+mypsh 'rsync -Pra /u/gmauro/data/references/ucsc/hg19/ucsc.hg19.* /tmp/ref/'
+start-cluster.sh
