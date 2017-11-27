e70fee30263dc4d74088aa18618d16196c4f2c50
Luca Pireddu <pireddu@crs4.it>
2017-11-27 12:23:12 +0100

    Workflow script for the experiment

---
+++ bin/workflow.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+OutputBase="."
+NumNodes=-1
+# The reference must exist on each slave node
+Reference="/tmp/ref/ucsc.hg19.fasta"
+
+
+# inside BITSCONF directory
+Header="ucsc19_cram_header"
+Properties="bclconverter.properties"
+
+function timestamp() {
+  date +"%Y:%m:%d-%H:%M:%S"
+  return 0
+}
+
+function log() {
+  echo $(timestamp) -- "${@}" >&2
+  return 0
+}
+
+function error() {
+  if [ $# -gt 0 ] ; then
+    log "${@}"
+  else
+    log "There was an error, but I don't know what happened"
+  fi
+  exit 1
+}
+  
+function usage_error() {
+  echo "USAGE: $0 { run_dir_path }+" >&2
+  exit 1
+}
+
+########### main ###########
+
+if [ $# -le 0 ]; then
+  usage_error
+fi
+
+if [ -z "${BITSCONF:-}" ]; then
+  error "BITSCONF environment variable undefined.  You must source 'source_me.sh' before running this workflow"
+fi
+
+if [ ! -d "${BITSCONF:-}" ]; then
+  error "BITSCONF environment variable doesn't point to a directory (${BITSCONF})"
+fi
+
+for run_dir in "${@}"; do
+  if [ ! -d "${run_dir}" -o ! -d "${run_dir}/raw" ]; then
+    error "Path ${run_dir} doesn't seem to be one of our run directories"
+  fi
+  if [ ! -f "${run_dir}/SampleSheet.csv" ]; then
+    error "Missing sample sheet file ${run_dir}/SampleSheet.csv"
+  fi
+done
+
+if [ "${NumNodes}" -le 0 ]; then
+  NumNodes=$(cat ${BITSCONF}/slaves | wc -l)
+  if [ "${NumNodes}" -le 0 ]; then # if NumNodes isn't an integer bash should generate an error
+    error "Couldn't get number of slave nodes"
+  fi
+fi
+
+HeaderFull="${BITSCONF}/${Header}"
+if [ ! -f "${HeaderFull}" ]; then
+  error "Missing header file ${HeaderFull}"
+fi
+
+PropertiesFull="${BITSCONF}/${Properties}"
+if [ ! -f "${PropertiesFull}" ]; then
+  error "Missing properties file ${PropertiesFull}"
+fi
+
+LogFile="workflow_log.start_$(timestamp)"
+
+# See all output (stdout and stderr) to tee which will also write it to the log file
+exec > >(tee "${LogFile}") 2>&1
+
+log "Running workflow on $# run directories"
+
+for run_dir in "${@}"; do
+  log "Starting with ${run_dir}"
+  time flink_pipe  "${run_dir}/raw" "${OutputBase}/$(basename "${run_dir}").cram" ${NumNodes} "${HeaderFull}" "${Reference}" "${PropertiesFull}"
+  log "Finished ${run_dir}"
+done
